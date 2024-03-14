fc564b54781544bb011a78f43c2f8ce51ebb450b
Jay Ann Rose Gerente <gerente.jayannrose.bscs2021@gmail.com>
2024-03-14 18:19:43 +0800

    added harvard resume

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,73 @@ function App() {
         <button onClick={handleDownload}>TEST</button>
       </div>
       {/* Place your resume here */}
+      <div className="container">
+        <div className="header">
+          <h1>JAY ANN ROSE GERENTE</h1>
+          <p>Web Designer</p>     
+        </div>
+        <div className="header-bottom">
+          <p>123 Anywhere St., Any City, ST 12345</p>
+          <p>09106128058 <span className="email">gerente.jayannrose.bscs2021@gmail.com</span></p>
+          <hr />
+        </div>
 
+        <div className="bottom">
+          <p className="text-header">Career Objective</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut officia consectetur veritatis officiis repudiandae laboriosam minus! Sunt at quod laboriosam ab quibusdam quidem excepturi, laudantium velit nulla cumque ratione maiores Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, cumque?</p>
+          <hr />
+
+          <p className="text-header">Achievements</p>
+          <div className="list-container">
+          <ul className="left-list">
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+          </ul>
+          <ul className="right-list">
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+          </ul>
+          </div>
+          <hr />
+
+          <p className="text-header">Education</p>
+          <strong><p>Bachelor of Science in Computer Science</p></strong>
+          <p>University of Caloocan City <strong>2020-2025</strong></p>
+          <hr />
+
+          <p className="text-header">Projects</p>
+          <ul className="left-list">
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+          </ul>
+          <ul className="right-list">
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+          </ul>
+          <hr />
+
+          <p className="text-header">Certifications</p>
+          <ul className="left-list">
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+          </ul>
+          <ul className="right-list">
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+          </ul>
+          <hr />
+
+          <p className="text-header">Skills</p>
+          <ul className="left-list">
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+          </ul>
+          <ul className="right-list">
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+          </ul>
+        </div>
+      </div>
       {/* ---------------------- */}
     </>
   );
